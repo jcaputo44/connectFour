66dac759ac43c753407483e7a05c4711f3472412
username <jcaputo66@aol.com>
2022-04-06 13:19:31 -0700

    layout looks good

--- js/main.js
+++ js/main.js
@@ -8,105 +8,105 @@ const chips = {
 
 //put in reverse order
 const columns = [
-  [35,28,21,14,7,0],
-  [36,29,22,15,8,1],
-  [37,30,23,16,9,2],
-  [38,31,24,17,10,3],
-  [39,32,25,18,11,4],
-  [40,33,26,19,12,5],
-  [41,34,27,20,13,6]
+  [35, 28, 21, 14, 7, 0],
+  [36, 29, 22, 15, 8, 1],
+  [37, 30, 23, 16, 9, 2],
+  [38, 31, 24, 17, 10, 3],
+  [39, 32, 25, 18, 11, 4],
+  [40, 33, 26, 19, 12, 5],
+  [41, 34, 27, 20, 13, 6]
 ]
 
 const winningCombos = [
-  [0,1,2,3],
-  [1,2,3,4],
-  [2,3,4,5],
-  [3,4,5,6],
+  [0, 1, 2, 3],
+  [1, 2, 3, 4],
+  [2, 3, 4, 5],
+  [3, 4, 5, 6],
 
-  [7,8,9,10],
-  [8,9,10,11],
-  [9,10,11,12],
-  [10,11,12,13],
+  [7, 8, 9, 10],
+  [8, 9, 10, 11],
+  [9, 10, 11, 12],
+  [10, 11, 12, 13],
 
-  [14,15,16,17],
-  [15,16,17,18],
-  [16,17,18,19],
-  [17,18,19,20],
+  [14, 15, 16, 17],
+  [15, 16, 17, 18],
+  [16, 17, 18, 19],
+  [17, 18, 19, 20],
 
-  [21,22,23,24],
-  [22,23,24,25],
-  [23,24,25,26],
-  [24,25,26,27],
+  [21, 22, 23, 24],
+  [22, 23, 24, 25],
+  [23, 24, 25, 26],
+  [24, 25, 26, 27],
 
-  [28,29,30,31],
-  [29,30,31,32],
-  [30,31,32,33],
-  [31,32,33,34],
+  [28, 29, 30, 31],
+  [29, 30, 31, 32],
+  [30, 31, 32, 33],
+  [31, 32, 33, 34],
 
-  [35,36,37,38],
-  [36,37,38,39],
-  [37,38,39,40],
-  [38,39,40,41],
+  [35, 36, 37, 38],
+  [36, 37, 38, 39],
+  [37, 38, 39, 40],
+  [38, 39, 40, 41],
 
-  [0,7,14,21],
-  [7,14,21,28],
-  [14,21,28,35],
+  [0, 7, 14, 21],
+  [7, 14, 21, 28],
+  [14, 21, 28, 35],
 
-  [1,8,15,22],
-  [8,15,22,29],
-  [15,22,29,36],
+  [1, 8, 15, 22],
+  [8, 15, 22, 29],
+  [15, 22, 29, 36],
 
-  [2,9,16,23],
-  [9,16,23,30],
-  [16,23,30,37],
+  [2, 9, 16, 23],
+  [9, 16, 23, 30],
+  [16, 23, 30, 37],
 
-  [3,10,17,24],
-  [10,17,24,31],
-  [17,24,31,38],
+  [3, 10, 17, 24],
+  [10, 17, 24, 31],
+  [17, 24, 31, 38],
 
-  [4,11,18,25],
-  [11,18,25,32],
-  [18,25,32,39],
-  
-  [5,12,19,26],
-  [12,19,26,33],
-  [19,26,33,40],
+  [4, 11, 18, 25],
+  [11, 18, 25, 32],
+  [18, 25, 32, 39],
 
-  [6,13,20,27],
-  [13,20,27,34],
-  [20,27,34,41],
+  [5, 12, 19, 26],
+  [12, 19, 26, 33],
+  [19, 26, 33, 40],
 
-  [14,22,30,38],
-  [7,15,23,31],
-  [15,23,31,39],
+  [6, 13, 20, 27],
+  [13, 20, 27, 34],
+  [20, 27, 34, 41],
 
-  [0,8,16,24],
-  [8,16,24,32],
-  [16,24,32,40],
+  [14, 22, 30, 38],
+  [7, 15, 23, 31],
+  [15, 23, 31, 39],
 
-  [1,9,17,25],
-  [9,17,25,33],
-  [17,25,33,41],
+  [0, 8, 16, 24],
+  [8, 16, 24, 32],
+  [16, 24, 32, 40],
 
-  [2,10,18,26],
-  [10,18,26,34],
-  [3,11,19,27],
+  [1, 9, 17, 25],
+  [9, 17, 25, 33],
+  [17, 25, 33, 41],
 
-  [20,26,32,38],
-  [13,19,25,31],
-  [19,25,31,37],
+  [2, 10, 18, 26],
+  [10, 18, 26, 34],
+  [3, 11, 19, 27],
 
-  [6,12,18,24],
-  [12,18,24,30],
-  [18,24,30,36],
+  [20, 26, 32, 38],
+  [13, 19, 25, 31],
+  [19, 25, 31, 37],
 
-  [5,11,17,23],
-  [11,17,23,29],
-  [17,23,29,35],
+  [6, 12, 18, 24],
+  [12, 18, 24, 30],
+  [18, 24, 30, 36],
 
-  [4,10,16,22],
-  [10,16,22,28],
-  [3,9,15,21],
+  [5, 11, 17, 23],
+  [11, 17, 23, 29],
+  [17, 23, 29, 35],
+
+  [4, 10, 16, 22],
+  [10, 16, 22, 28],
+  [3, 9, 15, 21],
 ];
 
 
@@ -131,33 +131,33 @@ const resetButton = document.getElementById('resetBtn');
 
 //----event listeners-----//
 
-resetButton.addEventListener('click', function(evt) {
+resetButton.addEventListener('click', function (evt) {
   init();
-}); 
+});
 
 document.querySelector('section.board')
-    .addEventListener('click', function(evt) {
-      const idx = circEls.indexOf(evt.target);
-      if (idx === -1 || board[idx] || 
+  .addEventListener('click', function (evt) {
+    const idx = circEls.indexOf(evt.target);
+    if (idx === -1 || board[idx] ||
       (winner !== null && winner !== undefined)) return;
-      // console.log(idx);
-      // console.log(evt.target);
-      for (let i = 0; i < columns.length; i++) {
-        for (let j = 0; j < columns[i].length; j++) {
-          if (idx === columns[i][j]) 
+    // console.log(idx);
+    // console.log(evt.target);
+    for (let i = 0; i < columns.length; i++) {
+      for (let j = 0; j < columns[i].length; j++) {
+        if (idx === columns[i][j])
           targetedColumn = columns[i];
-        }
       }
-      render();
-      turn = turn * -1;
-      winner = getWinner();
-      if (winner === 1) {
-        message.innerHTML = 'Player 1 Wins!'; 
-      } else if ( winner === -1) {
-        message.innerHTML = 'Player 2 Wins!';
-      } else {
-        message.innerHTML = '';
-      }
+    }
+    render();
+    turn = turn * -1;
+    winner = getWinner();
+    if (winner === 1) {
+      message.innerHTML = 'Player 1 Wins!';
+    } else if (winner === -1) {
+      message.innerHTML = 'Player 2 Wins!';
+    } else {
+      message.innerHTML = '';
+    }
 
   });
 
@@ -169,55 +169,54 @@ document.querySelector('section.board')
 init();
 
 function init() {
-    board = new Array (42).fill(null);
-    turn = 1;
-    winner = null; 
-    targetedColumn = [];
-    message.innerHTML = '';
-    circEls.forEach(function(circ) {
-      circ.style.backgroundColor = chips['null'];
-      p1Token.style.backgroundColor = 'rgb(2, 90, 255)';
-      p2Token.style.backgroundColor = 'rgb(2, 155, 2)';
-    });
-    render();
+  board = new Array(42).fill(null);
+  turn = 1;
+  winner = null;
+  targetedColumn = [];
+  message.innerHTML = '';
+  circEls.forEach(function (circ) {
+    circ.style.backgroundColor = chips['null'];
+    p1Token.style.backgroundColor = 'rgb(2, 90, 255)';
+    p2Token.style.backgroundColor = 'rgb(2, 155, 2)';
+  });
+  render();
 }
 
 
 
 function render() {
-    renderBoard(); 
-  
-  };
-  
-  function renderBoard() {
-    // console.log(targetedColumn);
-    // board.forEach(function(circ, idx) {
-      // circEls[idx].style.backgroundColor = chips[circ]; 
-      // targetedColumn.forEach(function(targ, idx){
-      for (let i = 0; i < targetedColumn.length; i++) {
-        // if (board[targetedColumn[i]] !== null) console.log('hello')
-        if (board[targetedColumn[i]] === null) {
-          board[targetedColumn[i]] = turn;
-          circEls[targetedColumn[i]].style.backgroundColor = chips[turn]; 
-          if (turn === -1) {
-            p2Token.style.backgroundColor = 'beige';
-            p1Token.style.backgroundColor = 'rgb(2, 90, 255)';
-          } else if (turn === 1) {
-            p1Token.style.backgroundColor = 'beige';
-            p2Token.style.backgroundColor = 'rgb(2, 155, 2)';
-          }
-            break;
-        }
-      } 
-  };
-  
-  function getWinner() {
-    for (let i = 0; i < winningCombos.length; i++) {
-      if (Math.abs(board[winningCombos[i][0]] + board[winningCombos[i][1]] + 
-        board[winningCombos[i][2]] + board[winningCombos[i][3]]) === 4)
-        return board[winningCombos[i][0]];
-      }
-  };
+  renderBoard();
 
-  
- 
+};
+
+function renderBoard() {
+  // console.log(targetedColumn);
+  // board.forEach(function(circ, idx) {
+  // circEls[idx].style.backgroundColor = chips[circ]; 
+  // targetedColumn.forEach(function(targ, idx){
+  for (let i = 0; i < targetedColumn.length; i++) {
+    // if (board[targetedColumn[i]] !== null) console.log('hello')
+    if (board[targetedColumn[i]] === null) {
+      board[targetedColumn[i]] = turn;
+      circEls[targetedColumn[i]].style.backgroundColor = chips[turn];
+      if (turn === -1) {
+        p2Token.style.backgroundColor = 'beige';
+        p1Token.style.backgroundColor = 'rgb(2, 90, 255)';
+      } else if (turn === 1) {
+        p1Token.style.backgroundColor = 'beige';
+        p2Token.style.backgroundColor = 'rgb(2, 155, 2)';
+      }
+      break;
+    }
+  }
+};
+
+function getWinner() {
+  for (let i = 0; i < winningCombos.length; i++) {
+    if (Math.abs(board[winningCombos[i][0]] + board[winningCombos[i][1]] +
+      board[winningCombos[i][2]] + board[winningCombos[i][3]]) === 4)
+      return board[winningCombos[i][0]];
+  }
+};
+
+
